9bffb0d7cfa1449ad74c8d95a5bdf112e577ae7f
H04329 <H04329@lab.aca>
2025-04-28 11:48:28 -0500

    final

---
+++ ejercicio_3.js
@@ -0,0 +1,12 @@
+const butOnClick = function() {
+    const nombre = document.getElementById("nombre")
+    const edad = document.getElementById("edad")
+    const divMensaje = document.getElementById("mensaje")
+    
+}
+
+const main = function() {
+    const boton = document.getElementById("guardar")
+    boton.addEventListener("click", butOnClick)
+}
+
